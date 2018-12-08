44683a6bc6de1d050d2815964d1a2e13b9dfb15c
381706-1-DenisovVladislavL <vlad.roix@example.com>
2018-12-08 22:35:43 +0300

    Added dirs for ArrList, link to project

---
+++ ArrList/main_ArrList.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "ArrList.h"
+
+int main()
+{
+	std::cout << "An example of using the ArrList will be implemented here" << std::endl;
+	return 0;
+}
---
+++ ArrListTest/ArrListTest.cpp
@@ -0,0 +1,8 @@
+#include "ArrList.h"
+
+#include <gtest.h>
+
+TEST(ArrList, throws_when_create_list_with_negative_size)
+{
+	//ASSERT_ANY_THROW(TQueue <int> V(-1));
+}
