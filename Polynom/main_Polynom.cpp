bb34294b1d79af6abfbd33bf25c472b24093067d
381706-1-DenisovVladislavL <vlad.roix@example.com>
2018-12-08 23:22:39 +0300

    Added dirs for Polynom

---
+++ Polynom/main_Polynom.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Polynom.h"
+
+int main()
+{
+	std::cout << "An example of using the Polynom will be implemented here" << std::endl;
+	return 0;
+}
---
+++ PolynomTest/PolynomTest.cpp
@@ -0,0 +1,8 @@
+#include "Polynom.h"
+
+#include <gtest.h>
+
+TEST(Polynom, throws_when_create_poly_with_negative_size)
+{
+	//ASSERT_ANY_THROW(TQueue <int> V(-1));
+}
