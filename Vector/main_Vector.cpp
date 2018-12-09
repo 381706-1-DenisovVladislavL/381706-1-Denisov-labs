7fb3df9e39cccc4c132c241f47d86d42aee195ab
381706-1-DenisovVladislavL <vlad.roix@example.com>
2018-12-09 21:28:33 +0300

    Edit main_Vector.cpp. Added example of use

--- Vector/main_Vector.cpp
+++ Vector/main_Vector.cpp
@@ -5,6 +5,50 @@ using namespace std;
 
 int main()
 {
-	cout << "An example of using the Vector will be implemented here" << endl;
+  cout << "An example of working with Vector."
+    << endl << "Denisov Vladislav L. Group 381706-1" << endl;
+
+  cout << "\n===============\n" << "Creating vectors..." << "\n===============\n";
+  TVector <int> V1(3), V2(3), R(3);
+  for (int i = 0; i < 3; i++)
+  {
+    V1[i] = (i + 1 - (i + 3) * 3);
+    V2[i] = (i * 10 + 5 - i) * 2;
+  }
+  cout << "V1: " << V1 << endl;
+  cout << "V2: " << V2 << endl;
+
+  cout << "\n===============\n" << "Examples of arithmetic operations:" << "\n===============" << endl;
+  try {
+    R = V1 + V2;
+    cout << "Result V1 + V2: " << R << endl;
+  }
+  catch (TException exp)
+  {
+    exp.Print();
+  }
+
+  try {
+    R = V1 - V2;
+    cout << "Result V1 - V2: " << R << endl;
+  }
+  catch (TException exp)
+  {
+    exp.Print();
+  }
+
+  try {
+    int k = V1 * V2;
+    cout << "Result V1 * V2: " << k << endl;
+  }
+  catch (TException exp)
+  {
+    exp.Print();
+  }
+
+  cout << "\n===============\n" << "An example of using input / output streams:" << "\n===============";
+  TVector <int> V(5);
+  cin >> V;
+  cout << "\nVector entered by you: " << V << endl;
 	return 0;
 }
