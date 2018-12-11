fd94f42cc9f4eb4e7a2e7fadca1bb336a67e600c
381706-1-DenisovVladislavL <vlad.roix@example.com>
2018-12-11 23:40:48 +0300

    Fix Stack for travis#3

--- Polynom/main_Polynom.cpp
+++ Polynom/main_Polynom.cpp
@@ -5,7 +5,7 @@
 int main()
 {
 	std::cout << "An example of using the Polynom will be implemented here" << std::endl;
-    TMonom A(3, 0, 2);
+   /* TMonom A(3, 0, 2);
     std::cin >> A; 
     std::cout << A;
     TMonom B(3, 0, 1);
@@ -13,7 +13,7 @@ int main()
     std::cout << B << std::endl;
     TMonom C(3, 0, 0);
     C = A * B;
-    std::cout << "Res: " << C << std::endl;
+    std::cout << "Res: " << C << std::endl;*/
   //  TMonom C(3, 0, 0);
    // C = A + B;
    // std::cout << C << "\n";
