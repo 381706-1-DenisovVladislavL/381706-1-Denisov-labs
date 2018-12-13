f12c65ea7529841b34001eb191486c878b892d26
381706-1-DenisovVladislavL <vlad.roix@example.com>
2018-12-13 22:46:29 +0300

    Fix for my beloved Travis <3

--- MatrixLib/Matrix.h
+++ MatrixLib/Matrix.h
@@ -98,28 +98,28 @@ TMatrix<T> TMatrix<T>::operator/(const TMatrix<T> &mt)
   if (this->size != mt.size)
     throw TException("Division of matrix of different dimensions.");
   double check = 1;
-  for (int i = 0; i < size; i++)
+  for (int i = 0; i < (*this).size; i++)
     check = check * (*this).mas[i][0];
   if (check < 0.000001)
     throw TException("The determinant of the matrix-right operand is zero.");
   TMatrix <T> copyMt(mt);
-  TMatrix <T> resMt(size);
-  for (int i = 0; i < size; i++) {
+  TMatrix <T> resMt((*this).size);
+  for (int i = 0; i < (*this).size; i++) {
     resMt[i][0] = 1;
     T k = copyMt[i][0];
-    for (int j = 0; j < size - i; j++)
+    for (int j = 0; j < (*this).size - i; j++)
     {
       copyMt[i][j] = copyMt[i][j] / k;
       resMt[i][j] = resMt[i][j] / k;
     }
   }
 
-  for (int j = 1; j < size; j++)
+  for (int j = 1; j < (*this).size; j++)
   {
     for (int i = 0; i < j; i++)
     {
       T temp = copyMt[i][j - i];
-      for (int k = j - i, c = 0; k < size - i; k++)
+      for (int k = j - i, c = 0; k < (*this).size - i; k++)
       {
         copyMt[i][k] = copyMt[i][k] - copyMt[j][c] * temp;
         resMt[i][k] = resMt[i][k] - resMt[j][c++] * temp;
