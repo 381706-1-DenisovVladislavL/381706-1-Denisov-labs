e41b24b22d2094897900ebc60cdbfd9029ad7a07
381706-1-DenisovVladislavL <vlad.roix@example.com>
2018-12-16 19:35:13 +0300

    Added lab Queue

--- QueueLib/Queue.h
+++ QueueLib/Queue.h
@@ -9,40 +9,54 @@ protected:
   int count;
 public:
   TQueue(int n = 0);
-  TQueue(TQueue <T> &Q);
+  TQueue(TQueue <T> &q);
   virtual ~TQueue();
 
-  void Put(T A);
+  void Put(T a);
   T Get();
   bool IsFull();
   bool IsEmpty();
+
+  void Print();
 };
 
 template <class T>
-TQueue<T>::TQueue(int n) : TStack<T>(n) { start = 0; count = 0; }
+TQueue<T>::TQueue(int n) : TStack<T>(n) 
+{ 
+  start = 0; 
+  count = 0; 
+}
 
 template <class T>
-TQueue<T>::TQueue(TQueue<T> &Q) : TStack<T>(Q) { start = Q.start; count = Q.count; }
+TQueue<T>::TQueue(TQueue<T> &q) : TStack<T>(q) 
+{ 
+  start = q.start; 
+  count = q.count; 
+}
 
 template <class T>
 TQueue<T>::~TQueue() {}
 
 template <class T>
-void TQueue<T>::Put(T A) {
+void TQueue<T>::Put(T a) 
+{
   if (IsFull()) 
-    throw 1;
-  else {
-    TStack<T>::mas[start] = A;
+    throw TException("Queue is full.");
+  else 
+  {
+    TStack<T>::mas[start] = a;
     start = (start + 1) % TStack<T>::size;
     count++;
   }  
 }
 
 template <class T>
-T TQueue<T>::Get() {
+T TQueue<T>::Get() 
+{
   if (IsEmpty()) 
-    throw 1;
-  else {
+    throw TException("Queue is empty.");
+  else 
+  {
     T temp = TStack<T>::mas[TStack<T>::top];
     TStack<T>::top = (TStack<T>::top + 1) % TStack<T>::size;
     count--;
@@ -51,7 +65,8 @@ T TQueue<T>::Get() {
 }
 
 template <class T>
-bool TQueue<T>::IsFull() {
+bool TQueue<T>::IsFull() 
+{
   if (count == TStack<T>::size)
     return true;
   else
@@ -59,9 +74,21 @@ bool TQueue<T>::IsFull() {
 }
 
 template <class T>
-bool TQueue<T>::IsEmpty() {
+bool TQueue<T>::IsEmpty() 
+{
   if (count == 0)
     return true;
   else 
     return false;
+}
+
+template <class T>
+void TQueue<T>::Print()
+{
+  if (TQueue<T>::IsEmpty())
+    throw TException("Queue is empty.");
+  std::cout << "\nExit <-";
+  for (int i = TStack<T>::top, j = 1; j <= count; i = (i + 1) % TStack<T>::size, j++)
+    std::cout << "\t|" << TStack<T>::mas[i] << "| <-";
+  std::cout << " Enter"<< std::endl;
 }
