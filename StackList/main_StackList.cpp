8c669078858d32dcaa1a203a6e9c8db012489075
381706-1-DenisovVladislavL <vlad.roix@example.com>
2018-12-19 22:10:23 +0300

    StackList is released

--- StackList/main_StackList.cpp
+++ StackList/main_StackList.cpp
@@ -1,10 +1,56 @@
 #include <iostream>
 #include "StackList.h"
 
+//Uncomment to use the stack I/O example.
+//#define __USE_EXAMPLE_IO__
+
 using namespace std;
 
 int main()
 {
-  cout << "An example of using the StackList will be implemented here" << endl;
+  cout << "An example of working with StackList."
+    << endl << "Denisov Vladislav L. Group 381706-1" << endl;
+
+  cout << "\nEnter the number of elements in the stack: ";
+  try
+  {
+    int n = 3;
+#ifdef __USE_EXAMPLE_IO__
+    cin >> n;
+#endif
+#ifndef __USE_EXAMPLE_IO__
+    cout << n << endl;
+#endif
+    TStackList <int> stack(n);
+    cout << "Enter the elements of the stack (use the gap): ";
+    for (int i = 0; i < n; i++)
+    {
+      int k;
+#ifdef __USE_EXAMPLE_IO__
+      cin >> k;
+#endif
+#ifndef __USE_EXAMPLE_IO__
+      k = i;
+      cout << k << " ";
+#endif
+      stack.Put(k);
+    }
+#ifndef __USE_EXAMPLE_IO__
+    cout << endl;
+#endif
+    stack.Print();
+
+    cout << "\nGet the element of the stack: " << stack.Get() << endl;
+
+    stack.Print();
+
+    TStackList <int> stackOther(stack);
+    cout << "\nCopy constructor (adress of oldStack: " << &stack << ", adress of newStack: " << &stackOther << "): " << endl;
+    stackOther.Print();
+  }
+  catch (TException exp)
+  {
+    exp.Print();
+  }
   return 0;
 }
--- StackListLib/StackList.h
+++ StackListLib/StackList.h
@@ -1,7 +1,93 @@
 #pragma once
-#include <iostream>
+
 #include "../ExceptionLib/Exception.h"
+#include "../ListLib/List.h"
 
 template <class T>
-class TStackList {
+class TStackList: protected TList <T> {
+protected:
+  int size;
+  int count;
+public:
+  TStackList(int _size = 5);
+  TStackList(TStackList<T> &s);
+  virtual ~TStackList();
+
+  void Put(T a);
+  T Get();
+
+  bool IsFull();
+  bool IsEmpty();
+
+  void Print();
 };
+
+template<class T>
+TStackList<T>::TStackList(int _size) : TList<T>()
+{
+  if (_size <= 0)
+    throw TException("Negative Stack size.");
+  size = _size;
+  count = 0;
+}
+
+template<class T>
+TStackList<T>::TStackList(TStackList<T> &s) : TList<T>(s)
+{
+  size = s.size;
+  count = s.count;
+}
+
+template<class T>
+TStackList<T>::~TStackList()
+{
+  size = 0;
+  count = 0;
+}
+
+template<class T>
+void TStackList<T>::Put(T a) 
+{
+  if (TStackList<T>::IsFull())
+    throw TException("Stack is full.");
+  TList<T>::PutBegin(a);
+  count++;
+}
+
+template<class T>
+T TStackList<T>::Get() 
+{
+  if (TStackList<T>::IsEmpty())
+    throw TException("Stack is empty.");
+  count--;
+  return TList<T>::GetBegin();
+}
+
+template <class T>
+bool TStackList<T>::IsFull() 
+{
+  if (size == count)
+    return true;
+  else
+    return false;
+}
+
+template <class T>
+bool TStackList<T>::IsEmpty()
+{
+  return TList<T>::IsEmpty();
+}
+
+template <class T>
+void TStackList<T>::Print() 
+{
+  if (TStackList<T>::IsEmpty())
+    throw TException("Stack is empty.");
+  TElem<T> *temp = begin;
+  do
+  {
+    std::cout << "\t|" << temp->TElem<T>::GetData() << "| " << std::endl;
+    temp = temp->GetNext();
+  } while (temp != 0);
+  std::cout << std::endl;
+}
