7bef2936f68d6a835fe2a28e20e1623f86cde8ac
381706-1-DenisovVladislavL <vlad.roix@example.com>
2019-02-10 15:52:43 +0300

    Disabled waiting for user to enter an expression.

--- Polish/main_Polish.cpp
+++ Polish/main_Polish.cpp
@@ -8,8 +8,9 @@ int main()
       << std::endl << "Denisov Vladislav L. Group 381706-1" << std::endl;
 
     std::cout << "\nEnter your math expression: ";
-    TString inputStr;
-    std::cin >> inputStr;
+    char arr[] = "16/4-(2+3)*4+(5-2)/2";
+    TString inputStr(arr);
+    //std::cin >> inputStr;
     TQueue<char> reversePolishNotation;
     reversePolishNotation = ConvertToPol(inputStr);
     std::cout << "\nYour entered expression:\n" << inputStr << "\nhas been converted to Reverse Polish Notation:\n";
