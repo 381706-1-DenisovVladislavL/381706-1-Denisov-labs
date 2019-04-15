e135b0f49c5aea6f2cf216149e8bac51fa37fa8d
381706-1-DenisovVladislavL <vlad.roix@example.com>
2019-04-15 20:32:58 +0300

    Added Plex

---
+++ Plex/main_Plex.cpp
@@ -0,0 +1,13 @@
+#include "Plex.h"
+
+int main()
+{
+  std::cout << "An example of working with Plex."
+    << std::endl << "Denisov Vladislav L. Group 381706-1\n" << std::endl;
+  
+  TPlex myplex();
+
+
+  
+  return 0;
+}
