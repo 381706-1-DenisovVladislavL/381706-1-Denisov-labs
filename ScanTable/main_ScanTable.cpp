2e643715e48b89c429043ec10e2a066d965a7c19
381706-1-DenisovVladislavL <vlad.roix@example.com>
2019-05-10 08:37:52 +0300

    start work with scantable

---
+++ ScanTable/main_ScanTable.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "ScanTable.h"
+
+//Uncomment to use the stack I/O example.
+//#define __USE_EXAMPLE_IO__
+
+using namespace std;
+
+int main()
+{
+  cout << "An example of working with ScanTable."
+    << endl << "Denisov Vladislav L. Group 381706-1" << endl;
+
+
+  return 0;
+}
---
+++ ScanTableTest/ScanTableTest.cpp
@@ -0,0 +1,9 @@
+#define _SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING
+
+#include <gtest.h>
+#include "ScanTable.h"
+
+//TEST(ScanTable, throws_when_create_queue_with_negative_size)
+//{
+//  ASSERT_ANY_THROW(TQueue <int> V(-1));
+//}
